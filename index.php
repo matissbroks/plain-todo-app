996816c310ea71bc66176699b7343f24594d88f6
matissbroks <matiss.broks@gmail.com>
2022-10-08 23:36:22 +0300

    Added CSS with fake ToDo items

--- index.php
+++ index.php
@@ -8,8 +8,50 @@
     <title>To-Do App</title>
 
     <link rel="stylesheet" href="css/main.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Plain To-Do App</h1>
+
+<?php
+    $todos = [
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4"
+    ];
+
+    $todosCount = count($todos);
+?>
+
+    <div class="container">
+        <h1>Plain To-Do App</h1>
+
+        <div class="content-box">
+            <div class="header">
+                <div class="grid-container">
+                    <div class="grid-item">
+                        Count of Todo's
+                    </div>
+
+                    <div class="grid-item">
+                        <?= $todosCount ?>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <ul class="todos-list">
+                    <?php foreach ($todos AS $todo) { ?>
+                        <li class="list-item">
+                            <?= $todo ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
