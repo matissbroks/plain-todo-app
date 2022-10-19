d4d66b4d06d3b1019009fe0f23890ce97e859578
matissbroks <matiss.broks@gmail.com>
2022-10-19 21:33:01 +0300

    Project startup. Added Bootstrap CDN. Removed custom CSS styles

--- index.php
+++ index.php
@@ -12,99 +12,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>To-Do App</title>
 
-    <link rel="stylesheet" href="css/main.css">
-
+    <!-- Fonts related -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap 4.4 related -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- Custom CSS file-->
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-
-<?php
-    $todos = [];
-    $todosSelectSQL = "SELECT * FROM todo";
-    $todosSelectSQL = $conn->query($todosSelectSQL);
-
-    while($todoRow = $todosSelectSQL->fetch_assoc()) {
-        $todos[] = $todoRow;
-    }
-
-    $conn->close();
-
-    $todosCount = count($todos);
-?>
-
-    <div class="container">
-        <h1><a href="index.php">Plain To-Do App</a></h1>
-
-        <div class="content-box">
-            <div class="header">
-                <div class="grid-container">
-                    <div class="grid-item">
-                        Count of Todo's
-                    </div>
-
-                    <div class="grid-item">
-                        <?= $todosCount ?>
-                    </div>
-                </div>
-            </div>
-
-
-            <form action="actions/add.php" method="POST">
-                <div class="grid-container input-form">
-                    <div class="grid-item">
-                        <input type="text" name="newTodo" placeholder="Do a Flip">
-                    </div>
-
-                    <div class="grid-item">
-                        <button class="btn add-btn" name="addTodo">Add</button>
-                    </div>
-                </div>
-            </form>
-
-            <?php if(isset($_GET['error']) || isset($_GET['success'])) { ?>
-                <?php
-                    if(isset($_GET['error'])) {
-                        $classToAdd = "info-clr-warning";
-                        $textToShow = $texts["error"][$_GET['error']];
-                    }
-                    else {
-                        $classToAdd = "info-clr-success";
-                        $textToShow = $texts["success"][$_GET['success']];
-                    }
-                ?>
-                <div class="grid-container info-box <?= $classToAdd ?>">
-                    <div class="grid-item info-text">
-                        <p><?= $textToShow ?></p>
-                    </div>
-                </div>
-            <?php } ?>
-
-            <div>
-                <ul class="todos-list">
-                    <?php if(!empty($todos)) { ?>
-                        <?php foreach ($todos AS $todo) { ?>
-                            <li class="list-item <?= ($todo["is_done"]) ? "done" : "" ?>" >
-                                <span class="todo">
-                                    <?= $todo["title"] ?>
-                                </span>
-
-                                <?php if(!$todo["is_done"]) { ?>
-                                    <a href="actions/markDone.php?id=<?= $todo["id"] ?>">
-                                        <button class="btn btn-done">Done</button>
-                                    </a>
-                                <?php } ?>
-
-                                <a href="actions/delete.php?id=<?= $todo["id"] ?>">
-                                    <button class="btn btn-delete">Delete</button>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    <?php } ?>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <h1>
+        <a href="index.php">Plain To-Do App | Bootstrap version</a>
+    </h1>
 </body>
 </html>
