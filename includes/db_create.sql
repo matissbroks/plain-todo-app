c2c1600f633d25e3796aa3b0918eac090795f2fa
matissbroks <matiss.broks@gmail.com>
2023-07-16 10:39:25 +0300

    Initial SQl script file created. Update db config, so it can use Docker DB container

---
+++ includes/db_create.sql
@@ -0,0 +1,10 @@
+-- Create main table
+create table todo
+(
+    id         int auto_increment
+        primary key,
+    title      varchar(255) charset utf8mb3         null,
+    is_done    tinyint(1) default 0                 null,
+    created_at timestamp  default CURRENT_TIMESTAMP null,
+    updated_at datetime   default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP
+);
